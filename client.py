4b1b0a2206cb722f40a0f921b7de36be506f1d09
AKASH CHODAVARAPU <chodavarapu.akash@gmail.com>
2017-02-28 17:34:21 -0800

    Update client.py

--- client.py
+++ client.py
@@ -7,23 +7,23 @@ class chatApp:
 	def __init__(self):	
 		self.host = socket.gethostname()
 		self.port = 12353
-        	self.s = socket.socket()
+        	self.s = socket.socket() # create socket
         	self.flag = 0
-		self.s.connect((self.host,self.port))
-                 
+		self.s.connect((self.host,self.port)) # connect to IP address
+        # send data to server         
 	def send(self):
 		while True:
 			k = raw_input()
 			print k
 			self.s.send(k)
 			continue
-	
+	# receive data from server
 	def receive(self):
 		while True:
 			k = self.s.recv(1024)	
 			print "\t"+k
 			continue
-
+	# create threads to send and receive data
     	def run(self):
         	t1 = threading.Thread(target=self.send)
        		t2 = threading.Thread(target=self.receive)
